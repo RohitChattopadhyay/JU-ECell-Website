f6b5e76d3c7c180326fd767f884aa68301c263a5
Daipayan Mukherjee <30791985+Satyr09@users.noreply.github.com>
2017-12-21 02:14:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1247,17 +1247,38 @@ Ideate. Innovate. Build!";
       <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
       <script async defer
          src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDRiDrm36Kcz2OWvZgnwFAB1-Lk9SIQOlY&callback=initMap"></script>
-      <script>
+   <script>
          function initMap() {
-           var uluru = {lat:22.560073 , lng: 88.413125};
-           var map = new google.maps.Map(document.getElementById('map'), {
-             zoom: 14,
-             center: uluru
-           });
-           var marker = new google.maps.Marker({
-             position: uluru,
-             map: map
-           });
+             
+            var locations = [
+              ['Jadavpur University<br>Salt Lake Campus', 22.560473, 88.413460, 2],
+              ['Jadavpur University', 22.499842, 88.371498, 1],
+            ];
+        
+            var map = new google.maps.Map(document.getElementById('map'), {
+              zoom: 12,
+              center: new google.maps.LatLng(22.5301575, 88.392479),
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
+        
+            var infowindow = new google.maps.InfoWindow();
+        
+            var marker, i;
+        
+            for (i = 0; i < locations.length; i++) {  
+              marker = new google.maps.Marker({
+                position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                icon: 'ecell.png',
+                map: map
+              });
+        
+              google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                  infowindow.setContent(locations[i][0]);
+                  infowindow.open(map, marker);
+                }
+              })(marker, i));
+            }
          }
       </script>
       <script>
