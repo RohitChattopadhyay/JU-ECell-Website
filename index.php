59712ea74b873766ecfdb46d4833370dce6cec73
Rohit Chattopadhyay <rohit.chattopadhyay1@gmail.com>
2017-12-20 12:15:54 +0530

    Charachter Encoding

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
    }
    
    ?>
+   
 <html>
    <head>
       <?php
@@ -69,7 +70,8 @@ Ideate. Innovate. Build!";
          		}
          }
          ?>
-      <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0' />
+      <meta charset="utf-8">
+	  <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0' />
       <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
       <link rel="stlyesheet"  href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
       <link rel="stylesheet" type="text/css" href="assets/style.css?noached">
