49d493b6230d208b34faf03b968dd8af264baf8c
Rohit Chattopadhyay <rohit.chattopadhyay1@gmail.com>
2017-12-20 17:44:24 +0530

    Title Added

--- index.php
+++ index.php
@@ -72,6 +72,9 @@ Ideate. Innovate. Build!";
          ?>
       <meta charset="utf-8">
 	  <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0' />
+	  
+	  <title>JU E-CELL | A Jadavpur University,Kolkata Students initiative</title>
+	  
       <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
       <link rel="stlyesheet"  href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
       <link rel="stylesheet" type="text/css" href="assets/style.css?noached">
