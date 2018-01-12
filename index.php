dc7de22fc5e18c53deb4cbb49a878a497384ed18
Rohit Chattopadhyay <rohit.chattopadhyay1@gmail.com>
2018-01-13 01:18:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -234,8 +234,8 @@ if (isset($_POST['contact-form-submit'])) {
         <div id="preloader">
 	<div id="status">&nbsp;</div>
 </div>
-        <button class="md-trigger" data-modal="form-modal" id="thank-you-modal-button" style="display:none"></button>
-        <div class="md-modal md-effect-4" id="form-modal">
+        <button class="md-trigger" data-modal="modal-11" id="thank-you-modal-button" style="display:none"></button>
+        <div class="md-modal md-effect-4" id="modal-11">
             <div class="md-content">
                 <h3>Thank You<a class="md-close" style="float:right ; text-decoration:none;color:white;cursor:pointer;">x</a></h3>
                 <div>
@@ -249,8 +249,8 @@ echo $_SESSION['message'];
         </div>
 	<!-- Normal Web Pop Up Modal
 	 trigger using document.getElementById('web-modal-button').click(); -->
-	<button class="md-trigger" data-modal="web-modal" id="web-modal-button" style="display:none"></button>
-	<div class="md-modal md-effect-4" id="web-modal">
+	<button class="md-trigger" data-modal="modal-12" id="web-modal-button" style="display:none"></button>
+	<div class="md-modal md-effect-4" id="modal-12">
             <div class="md-content">
                 <h3>Thank You<a class="md-close" style="float:right ; text-decoration:none;color:white;cursor:pointer;">x</a></h3>
                 <div>
